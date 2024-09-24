49cd9a41a9a7b20b140185ddd616b1bef1e2fcd8
Maciej Janicki <mjanicki@internships.antmicro.com>
2024-09-24 12:47:20 +0200

    Fix formatting of main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import yaml
 from itertools import product
 from subprocess import run
 
-with open("config.yml", 'r') as file:
+with open("config.yml", "r") as file:
     config = yaml.safe_load(file)
 
 
@@ -12,27 +12,27 @@ def product_dict(**kwargs):
         yield dict(zip(keys, instance))
 
 
-var_combinations = list(product_dict(**config['matrix']))
+var_combinations = list(product_dict(**config["matrix"]))
 
 
 def prepare_command(command: str, var_combination) -> list:
     for var in var_combination:
         command = command.replace(f"$matrix.{var}", str(var_combination[var]))
-    subcommands = command.split(' ')
+    subcommands = command.split(" ")
     return subcommands
 
 
 for var_combination in var_combinations:
     print(f"Exectuting with variables set to: {var_combination}.")
-    for command in config['run']['before']:
+    for command in config["run"]["before"]:
         c = prepare_command(command, var_combination)
         print(f"Running {' '.join(c)}")
         run(c, shell=True)
-    for command in config['run']['benchmark']:
+    for command in config["run"]["benchmark"]:
         c = prepare_command(command, var_combination)
         print(f"Running {' '.join(c)}")
         run(c, shell=True)
-    for command in config['run']['after']:
+    for command in config["run"]["after"]:
         c = prepare_command(command, var_combination)
         print(f"Running {' '.join(c)}")
         run(c, shell=True)
