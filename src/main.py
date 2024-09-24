27016d000d75c378212fb95d13fd2d77ddb312b0
Maciej Janicki <mjanicki@internships.antmicro.com>
2024-09-24 11:09:12 +0200

    Substituting variables with values in commands from config

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import yaml
 from itertools import product
+
 with open("config.yml", 'r') as file:
     config = yaml.safe_load(file)
 
@@ -10,6 +11,22 @@ def product_dict(**kwargs):
         yield dict(zip(keys, instance))
 
 
-combinations = list(product_dict(**config['matrix']))
-print(combinations)
-print(len(combinations))
+var_combinations = list(product_dict(**config['matrix']))
+
+
+def prepare_command(command: str, var_combination) -> list:
+    for var in var_combination:
+        command = command.replace(f"$matrix.{var}", str(var_combination[var]))
+    subcommands = command.split(' ')
+    return subcommands
+
+
+for var_combination in var_combinations:
+    print(f"Exectuting with variables set to: {var_combination}.")
+    for command in config['run']['before']:
+        print(prepare_command(command, var_combination))
+    for command in config['run']['benchmark']:
+        print(prepare_command(command, var_combination))
+    for command in config['run']['after']:
+        print(prepare_command(command, var_combination))
+    print()
