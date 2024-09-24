ccf6c86b02e5d3f2a7e2a8c4b51bee3d954b91f8
Maciej Janicki <mjanicki@internships.antmicro.com>
2024-09-24 10:50:36 +0200

    Creating combinations of all possible variable values

--- src/main.py
+++ src/main.py
@@ -1,4 +1,15 @@
 import yaml
+from itertools import product
 with open("config.yml", 'r') as file:
     config = yaml.safe_load(file)
-print(config)
+
+
+def product_dict(**kwargs):
+    keys = kwargs.keys()
+    for instance in product(*kwargs.values()):
+        yield dict(zip(keys, instance))
+
+
+combinations = list(product_dict(**config['matrix']))
+print(combinations)
+print(len(combinations))
