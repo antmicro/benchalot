06f80a5a70f252abf03c6c2476706ad1c834e52e
Maciej Janicki <mjanicki@internships.antmicro.com>
2024-09-26 16:56:32 +0200

    Initial impelementation using Schema

--- src/validation.py
+++ src/validation.py
@@ -1,5 +1,5 @@
-from re import findall
 from sys import stderr
+from schema import Schema, Optional, And, Or
 
 
 def error(msg):
@@ -8,50 +8,27 @@ def error(msg):
 
 
 def validate_config(config):
+    reqs_for_commands = And(list, lambda x: len(x) > 0)
+    config_schema = Schema({
+        Optional("matrix"):{ 
+            str: And(list, lambda x: len(x) > 0 ) # if matrix section exists it should contain variables which are non empty lists
+        },
+        "run": {
+            "benchmark": reqs_for_commands,
+            Optional("before"): reqs_for_commands,
+            Optional("after"): reqs_for_commands
+        },
+        "output": {
+            str: Or({"format":"csv",
+                        "filename":str},
+                       {"format":"table-md",
+                        "filename":str,
+                        Optional("columns"): And(list, lambda x: len(x) > 0)})
+                        
+        
+        }
+    })
 
-    if "run" not in config:
-        error("`run` section not found.")
-    if "benchmark" not in config["run"]:
-        error("'run.benchmark' section not found.")
-    for section in ["before", "benchmark", "after"]:
-        if section not in config["run"]:
-            continue
-        for command in config["run"][section]:
-            variables = findall(r"\$matrix\.[a-zA-Z0-9]*", command)
-            for var in variables:
-                var_key = var.split(".")[1]
-                if "matrix" not in config or var_key not in config["matrix"]:
-                    error(
-                        f"Variable `{var}` in the command `{command}` not found in the `matrix` section."
-                    )
+    print(config_schema.validate(config))
 
-    if "output" not in config or config["output"] is None:
-        error("`output` section not found.")
-
-    for key in config["output"]:
-        output = config["output"][key]
-        if "filename" not in output:
-            error(f"`filename` section not found in `output.{key}`.")
-        if "format" not in output:
-            error(f"`format` section not found in `output.{key}`.")
-        if output["format"] == "bar-chart":
-            if "matrix" not in config:
-                error("`matrix` section not found.")
-            if "x-axis" not in output:
-                error(f"`x-axis` section not found in `output.{key}`.")
-            for option in ["facet", "color", "x-axis"]:
-                if option not in config["output"][key]:
-                    continue
-                if output[option] not in config["matrix"]:
-                    error(
-                        f"Variable `{output[option]}` in the `output.{key}.{option}` not found in the `matrix` section."
-                    )
-        elif output["format"] == "table-md":
-            if "columns" in output:
-                if output["columns"] is None or len(output["columns"]) == 0:
-                    error("No columns specified in `output.columns`.")
-                for var in output["columns"]:
-                    if var not in config["matrix"]:
-                        error(
-                            f"Variable `{var}` in the `output.{key}.columns` not found in the `matrix` section."
-                        )
+    return
