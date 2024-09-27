e368bf5f04493508f2adcf55c95d2cb78fa87e8c
Maciej Janicki <mjanicki@internships.antmicro.com>
2024-09-27 13:27:42 +0200

    Slow sleeper

--- main.c
+++ main.c
@@ -6,11 +6,11 @@ int main(int argc, const char **argv) {
   int n_threads = atoi(argv[1]);
   int sleep_time;
   if(strcmp(argv[2],"data1") == 0) {
-    sleep_time = 1000000;
-  } else if(strcmp(argv[2],"data2") == 0) { 
-    sleep_time = 500000;
-  } else if(strcmp(argv[2],"data3") == 0) { 
     sleep_time = 1400000;
+  } else if(strcmp(argv[2],"data2") == 0) { 
+    sleep_time = 520000;
+  } else if(strcmp(argv[2],"data3") == 0) { 
+    sleep_time = 1500000;
   }
   usleep(sleep_time/n_threads);
   return 0;
