599fb49b88b399a04e4928c60780fdf8c8cab67d
Maciej Janicki <mjanicki@internships.antmicro.com>
2024-10-01 15:50:58 +0200

    Feature: execution file logging

--- src/execution.py
+++ src/execution.py
@@ -1,26 +1,58 @@
 from time import monotonic_ns
 from subprocess import run
 from yaspin import yaspin
-from logging import getLogger
+from logging import getLogger, FileHandler, Formatter, INFO
+
+formatter = Formatter("%(levelname)s:%(message)s")
 
 logger = getLogger(__name__)
+handler = FileHandler("execution.log")
+handler.setFormatter(formatter)
+command_logger = getLogger(__name__)
+command_logger.setLevel(INFO)
+command_logger.addHandler(handler)
+
+
+def log_run_results(result):
+    msg = f"Executed: {result.args}"
+    if len(result.stdout) > 0:
+        msg += f"\nstdout:\n{result.stdout}"
+    if len(result.stderr) > 0:
+        msg += f"\nstderr:\n{result.stderr}"
+    command_logger.info(msg)
 
 
 def run_multiple_commands(commands: list):
     for c in commands:
-        result = run(c, shell=True)
+        result = run(c, shell=True, capture_output=True, text=True)
         if result.returncode != 0:
             print(
                 f"Subprocess `{c}` exited abnormally (exit code: {result.returncode})"
             )
+            print(f"stderr: {str(result.stderr)}")
             exit(1)
+        log_run_results(result)
+
+
+def measure_time_command(command: str) -> tuple:
+    start = monotonic_ns()
+    result = run(command, shell=True, capture_output=True)
+    return (monotonic_ns() - start), result
 
 
 def benchmark_commands(commands: list) -> float:
+    total = 0
     with yaspin(text=f"Benchmarking {commands}...", timer=True):
-        start = monotonic_ns()
-        run_multiple_commands(commands)
-        return (monotonic_ns() - start) / 1e9  # convert to seconds
+        for command in commands:
+            time, result = measure_time_command(command)
+            if result.returncode != 0:
+                print(
+                    f"Subprocess `{command}` exited abnormally (exit code: {result.returncode})"
+                )
+                exit(1)
+            log_run_results(result)
+            total += time
+    return total / 1e9  # convert to seconds
 
 
 def perform_benchmarks(benchmarks: list, samples: int) -> list:
