526d54be67d3fdfcc5c7707917860e75e400013f
Maciej Janicki <mjanicki@internships.antmicro.com>
2024-10-04 12:03:04 +0200

    Update: set scaling governor without external dependency. Restore system state

--- src/variance.py
+++ src/variance.py
@@ -1,19 +1,40 @@
 from atexit import register, unregister
 from subprocess import run
 from sys import stderr
+from multiprocessing import cpu_count
+
+
+def get_and_set(filename: str, value: str):
+    try:
+        file = open(filename, "r")
+        ret = file.read()
+        file.close()
+        set(filename, value)
+    except FileNotFoundError:
+        print(f"ERROR: Failed to set {filename} to {value}")
+        exit(1)
+    return ret
+
+
+def set(filename: str, value: str):
+    try:
+        file = open(filename, "w")
+        file.write(value)
+        file.close()
+    except FileNotFoundError as e:
+        print(f"ERROR: Failed to set {filename} to {value} {e.strerror}")
+        exit(1)
+
+
+system_state = {}
 
 
 def modify_system_state(system_options):
     register(restore_system_state, system_options)
     if system_options.get("disable-aslr"):
-        try:
-            aslr_file = open("/proc/sys/kernel/randomize_va_space", "w")
-            aslr_file.write(str(0))
-            aslr_file.close()
-        except FileNotFoundError:
-            print("ERROR: Failed to disable ASLR.")
-            exit(1)
-
+        system_state["aslr"] = get_and_set(
+            "/proc/sys/kernel/randomize_va_space", str(0)
+        )
     if "isolate-cpus" in system_options:
         cpu_str = ""
         for cpu in system_options["isolate-cpus"]:
@@ -26,43 +47,42 @@ def modify_system_state(system_options):
                 file=stderr,
             )
             exit(1)
-        if (
-            "governor-performance" in system_options
-            and system_options["governor-performance"]
-        ):
-            result = run(
-                f"cpupower --cpu {cpu_str} frequency-set --governor performance",
-                shell=True,
-            )
-
-            if result.returncode != 0:
-                print(
-                    f"ERROR: Failed to set CPUs {cpu_str} frequency governor to performance (exit code {result.returncode})",
-                    file=stderr,
+        if system_options.get("governor-performance"):
+            for cpu in system_options["isolate-cpus"]:
+                system_state[f"governor{cpu}"] = get_and_set(
+                    f"/sys/devices/system/cpu/cpu{cpu}/cpufreq/scaling_governor",
+                    "performance",
                 )
-                exit(1)
-
-    elif (
-        "governor-performance" in system_options
-        and system_options["governor-performance"]
-    ):
-        result = run("cpupower frequency-set --governor performance", shell=True)
-        if result.returncode != 0:
-            print(
-                f"ERROR: Failed to set CPUs frequency governor to performance (exit code {result.returncode})",
-                file=stderr,
+    elif system_options.get("governor-performance"):
+        for cpu in range(cpu_count()):
+            system_state[f"governor{cpu}"] = get_and_set(
+                f"/sys/devices/system/cpu/cpu{cpu}/cpufreq/scaling_governor",
+                "performance",
             )
-            exit(1)
 
 
 def restore_system_state(system_options):
     if "isolate-cpus" in system_options:
         run("cset shield --reset", shell=True)
+        if system_options.get("governor-performance"):
+            for cpu in system_options["isolate-cpus"]:
+                set(
+                    f"/sys/devices/system/cpu/cpu{cpu}/cpufreq/scaling_governor",
+                    system_state[f"governor{cpu}"],
+                )
+    elif system_options.get("governor-performance"):
+        for cpu in range(cpu_count()):
+            set(
+                f"/sys/devices/system/cpu/cpu{cpu}/cpufreq/scaling_governor",
+                system_state[f"governor{cpu}"],
+            )
+
     if system_options.get("disable-aslr"):
         try:
             aslr_file = open("/proc/sys/kernel/randomize_va_space", "w")
-            aslr_file.write(str(2))
+            aslr_file.write(system_state["aslr"])
             aslr_file.close()
         except FileNotFoundError:
             print("ERROR: Failed to restore state of ASLR.")
+
     unregister(restore_system_state)
