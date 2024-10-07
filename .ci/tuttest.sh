c7e48655a247dd2cdb4fc82973aaeb4d03f7a918
Maciej Janicki <mjanicki@internships.antmicro.com>
2024-10-07 15:44:45 +0200

    Fix: revert to using functions with error msgs

--- .ci/tuttest.sh
+++ .ci/tuttest.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+assert_file_exists() {
+    if [ ! -f "$1" ]; then
+        echo "$1 does not exist."
+        exit 1
+    fi
+}
+
 TMP_DIR=$(mktemp -d)
 git clone . $TMP_DIR
 cd $TMP_DIR
@@ -14,17 +21,21 @@ if [ "$CI" == 'true' ]; then
 fi
 eval "$INSTALL"
 
+rm config.yml
 echo "$CONFIG" > config.yml
 
 eval "$RUN"
-[ -f "plot.png" ] || exit 1
-[ -f "table.md" ] || exit 1
-[ -f "result.csv" ] || exit 1
+assert_file_exists plot.png
+assert_file_exists table.md
+assert_file_exists result.csv
 mv plot.png previous.png
 
 printf "  cs2:\n    filename: \"result2.csv\" \n    format: \"csv\"" >> config.yml
 eval "$RUN_UPDATE"
-[ -f "result2.csv" ] || exit 1
+assert_file_exists result2.csv
 cmp plot.png previous.png
-
-[ $? -ne 0 ] || exit 1
+ret=$?
+if [ $ret -ne 0 ]; then
+    echo "Plots are not the same."
+    exit 1
+fi
