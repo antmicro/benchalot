e64219975215aad334d992068adbec76d2392641
Maciej Janicki <mjanicki@internships.antmicro.com>
2024-10-14 11:24:20 +0200

    Fix: use mean instead of max in bar-charts

--- src/output.py
+++ src/output.py
@@ -2,6 +2,7 @@ from plotnine import ggplot, aes, geom_bar, facet_grid, theme_classic, labs
 import pandas as pd
 from logging import getLogger
 from datetime import timezone, datetime
+from numpy import mean
 
 logger = getLogger(f"benchmarker.{__name__}")
 
@@ -65,7 +66,7 @@ def output_results(results_df: pd.DataFrame, config: dict):
                         y=RESULTS_COLUMN,
                     ),
                 )
-                + geom_bar(position="dodge", stat="identity")
+                + geom_bar(position="dodge", stat="summary", fun_y=mean)
                 + theme_classic()
                 + labs(x=output["x-axis"])
             )
