b65b5a3516261426477d530192ef6e5ec8f0cca8
Maciej Janicki <mjanicki@internships.antmicro.com>
2024-10-22 15:03:58 +0200

    Feature: initial validation using pydantic

--- src/benchmarker/validation.py
+++ src/benchmarker/validation.py
@@ -1,6 +1,7 @@
-# Cerberus does not provide stubs for mypy
-from cerberus import Validator  # type: ignore
-from re import findall
+from pydantic import BaseModel, ValidationError, ConfigDict, Field, field_validator
+from typing import Literal
+
+# from re import findall
 from logging import getLogger
 from os import getcwd
 from os.path import isdir
@@ -9,280 +10,72 @@ logger = getLogger(f"benchmarker.{__name__}")
 
 
 def error_and_exit(error):
-    logger.critical("Config validation failed:")
+    logger.critical("Config validation failed.")
     logger.critical(error)
     exit(1)
 
 
-def validate_config(config) -> dict:
-    def variable_exists(field, value, error):
-        if (
-            "matrix" not in config or value not in config["matrix"]
-        ) and value != "benchmark_date":
-            error(field, f"variable `{value}` does not exist")
+class SystemSection(BaseModel):
+    isolate_cpus: list[int] = Field(default=None, alias="isolate-cpus")
+    disable_aslr: bool = Field(default=False, alias="disable-aslr")
+    governor_performance: bool = Field(default=False, alias="governor-performance")
+    model_config = ConfigDict(extra="forbid")
 
-    def variables_exist(field, value, error):
-        for var in value:
-            variable_exists(field, var, error)
 
-    def check_command_variables(field, value, error):
-        if "matrix" not in config:
-            return
-        # unfortunately cerberus does not stop checking on first validation error.
-        if value is None:
-            error(field, "no commands")
-            return
-        for command in value:
-            variables = findall(r"\$matrix\.[a-zA-Z0-9]*", command)
-            for var in variables:
-                var_key = var.split(".")[1]
-                variable_exists(field, var_key, error)
+class RunSection(BaseModel):
+    samples: int = 1
+    save_output: str = Field(default=None, alias="save-output")
+    before_all: list[str] = Field(default=[], alias="before-all")
+    before: list[str] = []
+    benchmark: list[str]
+    after: list[str] = []
+    after_all: list[str] = Field(default=[], alias="after-all")
+    cwd: str = getcwd()
+    metrics: list[str | dict] = ["time"]
 
-    def at_least_one_output_csv(field, value, error):
-        if value is None:
-            error(field, "no output specified in the `output` section")
-            return
-        has_csv = False
-        for output in value:
-            if value[output]["format"] == "csv":
-                has_csv = True
-                break
-        if not has_csv:
-            error(field, "has to have at least one `.csv` output")
-
-    def metric_exists(field, value, error):
-        if value is None:
-            return
-        if "metrics" not in config["run"]:
-            if value != "time":
-                error(field, f"did not find metric '{value}'")
-            return
-        if value not in [
-            metric if type(metric) is not dict else list(metric.keys())[0]
-            for metric in config["run"]["metrics"]
-        ]:
-            error(field, f"did not find metric '{value}'")
-            return
-
-    def supported_metrics(field, value, error):
-        for metric in value:
-            if metric not in ["time", "stdout", "stderr"]:
-                if type(metric) is not dict:
-                    error(field, f"invalid metric {metric}")
-
-    def path_exists(field, value, error):
-        if value is None:
-            return
+    @field_validator("cwd")
+    @classmethod
+    def path_exists(cls, value: str):
         if not isdir(value):
-            error(field, f"directory '{value}' not found")
-            return
+            raise ValueError(f"directory '{value}' not found")
+        return value
 
-    valid_schema = {
-        "matrix": {
-            "required": False,
-            "type": "dict",
-            "valuesrules": {"type": "list", "empty": False},
-            "empty": False,
-        },
-        "system": {
-            "required": False,
-            "type": "dict",
-            "schema": {
-                "isolate-cpus": {
-                    "type": "list",
-                    "empty": False,
-                    "required": False,
-                },
-                "disable-aslr": {
-                    "type": "boolean",
-                    "required": False,
-                },
-                "governor-performance": {"type": "boolean", "required": False},
-            },
-        },
-        "run": {
-            "required": True,
-            "type": "dict",
-            "schema": {
-                "samples": {
-                    "type": "integer",
-                    "required": False,
-                    "min": 1,
-                    "default": 1,
-                },
-                "before-all": {
-                    "required": False,
-                    "type": "list",
-                    "empty": False,
-                    "minlength": 1,
-                },
-                "before": {
-                    "required": False,
-                    "type": "list",
-                    "empty": False,
-                    "minlength": 1,
-                    "check_with": check_command_variables,
-                },
-                "benchmark": {
-                    "required": True,
-                    "type": "list",
-                    "empty": False,
-                    "minlength": 1,
-                    "check_with": check_command_variables,
-                },
-                "after": {
-                    "required": False,
-                    "type": "list",
-                    "empty": False,
-                    "check_with": check_command_variables,
-                },
-                "after-all": {
-                    "required": False,
-                    "type": "list",
-                    "empty": False,
-                    "minlength": 1,
-                },
-                "save-output": {
-                    "required": False,
-                    "type": "string",
-                    "empty": False,
-                },
-                "metrics": {
-                    "type": "list",
-                    "empty": False,
-                    "required": False,
-                    "default": ["time"],
-                    "check_with": supported_metrics,
-                },
-                "cwd": {
-                    "required": False,
-                    "type": "string",
-                    "empty": False,
-                    "default": getcwd(),
-                    "check_with": path_exists,
-                },
-            },
-        },
-        "output": {
-            "required": True,
-            "type": "dict",
-            "valuesrules": {
-                "oneof": [
-                    {
-                        "schema": {
-                            "filename": {"type": "string", "empty": False},
-                            "format": {"type": "string", "allowed": ["csv"]},
-                        },
-                    },
-                    {
-                        "schema": {
-                            "filename": {"type": "string", "empty": False},
-                            "format": {
-                                "type": "string",
-                                "allowed": ["table-md"],
-                            },
-                            "columns": {
-                                "type": "list",
-                                "empty": False,
-                                "required": False,
-                                "dependencies": "^matrix",
-                                "check_with": variables_exist,
-                            },
-                            "result-column": {
-                                "type": "string",
-                                "empty": False,
-                                "required": False,
-                                "check_with": metric_exists,
-                            },
-                        },
-                    },
-                    {
-                        "schema": {
-                            "filename": {"type": "string", "empty": False},
-                            "format": {
-                                "type": "string",
-                                "allowed": ["bar-chart"],
-                            },
-                            "x-axis": {
-                                "type": "string",
-                                "empty": False,
-                                "required": True,
-                                "dependencies": "^matrix",
-                                "check_with": variable_exists,
-                            },
-                            "y-axis": {
-                                "type": "string",
-                                "empty": False,
-                                "required": False,
-                                "check_with": metric_exists,
-                            },
-                            "color": {
-                                "type": "string",
-                                "empty": False,
-                                "required": False,
-                                "dependencies": "^matrix",
-                                "check_with": variable_exists,
-                            },
-                            "facet": {
-                                "type": "string",
-                                "empty": False,
-                                "required": False,
-                                "dependencies": "^matrix",
-                                "check_with": variable_exists,
-                            },
-                            "width": {
-                                "type": "integer",
-                                "min": 1,
-                                "required": False,
-                            },
-                            "height": {
-                                "type": "integer",
-                                "min": 1,
-                                "required": False,
-                            },
-                            "dpi": {
-                                "type": "integer",
-                                "min": 10,
-                                "required": False,
-                            },
-                        }
-                    },
-                ],
-                "empty": False,
-            },
-            "check_with": at_least_one_output_csv,
-        },
-    }
-    logger.info("Validating config...")
-    logger.debug(f"Config: {config}")
-    v = Validator(schema=valid_schema, require_all=True)
-    if not v.validate(config):
-        error_and_exit(v.errors)
-    logger.info("Finished validating config.")
-    normalized_config = v.normalized(config)
 
-    # Set default values for "result-column" and "y-axis"
-    # We have do it manually because default values for fields
-    # do not apply correctly when fields are specified in "valuesrules"
-    defaultable_fields = {"table-md": "result-column", "bar-chart": "y-axis"}
-    for key in normalized_config["output"]:
-        output = normalized_config["output"][key]
-        if (
-            output["format"] in defaultable_fields
-            and defaultable_fields[output["format"]] not in output
-        ):
-            if len(normalized_config["run"]["metrics"]) > 1:
-                error_and_exit(
-                    f"'{key}': no '{defaultable_fields[output['format']]}' specified."
-                )
-            metric = normalized_config["run"]["metrics"][0]
-            if type(metric) is dict:
-                metric = list(metric.keys())[0]
-            output[defaultable_fields[output["format"]]] = metric
-        if output["format"] == "table-md" and "columns" not in output:
-            if "matrix" in normalized_config:
-                output["columns"] = list(normalized_config["matrix"].keys())
-            else:
-                output["columns"] = None
-    logger.debug(f"Normalized config {normalized_config}")
-    return normalized_config
+class OutputField(BaseModel):
+    filename: str
+    format: str
+
+
+class CsvOutput(OutputField):
+    format: Literal["csv"]
+
+
+class BarChartOutput(OutputField):
+    format: Literal["bar-chart"]
+    x_axis: str = Field(alias="x-axis")
+    y_axis: str = Field(default="time", alias="y-axis")
+    facet: str | None = None
+    width: int = Field(default=10, ge=1)
+    height: int = Field(default=9, ge=1)
+    dpi: int = Field(default=100, ge=50)
+
+
+class TableMdOutput(OutputField):
+    format: Literal["table-md"]
+    columns: list[str] = []
+
+
+class ConfigFile(BaseModel):
+    matrix: dict[str, list] | None = None
+    system: SystemSection | None = None
+    run: RunSection
+    output: dict[str, CsvOutput | BarChartOutput | TableMdOutput]
+
+
+def validate_config(config) -> dict:
+    try:
+        ConfigFile(**config)
+    except ValidationError as e:
+        error_and_exit(e)
+    print("Validation succesful!")
+    exit()
