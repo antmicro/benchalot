96964635e4487fa8fad5f854423e1497a2657164
Maciej Janicki <mjanicki@internships.antmicro.com>
2024-11-19 16:16:32 +0100

    Feature: initial implementation of taking all measurements at once

--- src/benchmarker/metrics.py
+++ src/benchmarker/metrics.py
@@ -1,12 +1,10 @@
 from time import monotonic_ns
 from logging import getLogger
 from benchmarker.execution import (
-    execute_and_handle_output,
     handle_output,
     check_return_code,
     execute_command,
 )
-from typing import Literal
 from io import StringIO
 from csv import DictReader
 from benchmarker.structs import BenchmarkResult
@@ -15,93 +13,58 @@ logger = getLogger(f"benchmarker.{__name__}")
 command_logger = getLogger("run")
 
 
-def measure_time(benchmarks: dict[str, list[str]]) -> BenchmarkResult:
-    """Measure execution time of the commands.
-
-    Args:
-        benchmarks: Commands divided into stages.
-
-    Returns:
-        BenchmarkResult: Containing execution time of each stage.
-    """
+def execute_benchmark(
+    benchmarks: dict[str, list[str]], builtin_metrics, custom_metrics
+) -> list[BenchmarkResult]:
     has_failed = False
+    measure_time = "time" in builtin_metrics
+    gather_stdout = "stdout" in builtin_metrics
+    gather_stderr = "stderr" in builtin_metrics
 
-    def _measure_stage_time(commands: list[str]) -> float:
-        nonlocal has_failed
+    time_measurements = {}
+    output_measurements = {}
+
+    for stage in benchmarks:
         elapsed_time = 0.0
-        for command in commands:
+        output = ""
+        for command in benchmarks[stage]:
             start = monotonic_ns()
             process = execute_command(command)
             code = process.wait()
             elapsed_time += monotonic_ns() - start
-            handle_output(process)
+            output += handle_output(
+                process, capture_stdout=gather_stdout, capture_stderr=gather_stderr
+            )
             success = check_return_code(command, code)
             if not success:
                 has_failed = True
-        return elapsed_time / 1e9
+        if measure_time:
+            time_measurements[stage] = elapsed_time / 1e9
+        if gather_stderr or gather_stdout:
+            try:
+                output_measurements[stage] = float(output)
+            except ValueError:
+                logger.error("Conversion of '{output}' to float failed.'")
 
-    measurements: dict[str, float] = {}
-    for stage, commands in benchmarks.items():
-        measurements[stage] = _measure_stage_time(commands)
-    result = BenchmarkResult("time", has_failed, measurements)
-    return result
+    benchmark_results: list[BenchmarkResult] = []
 
+    for metric_name, command in custom_metrics:
+        custom_measurements = custom_metric(command)
+        benchmark_results.append(
+            BenchmarkResult(metric_name, has_failed, custom_measurements)
+        )
 
-def _gather_output(
-    benchmarks: dict[str, list[str]], output: Literal["stderr", "stdout"]
-) -> BenchmarkResult:
-    """Gather `stdout` or `stderr` of each command in each stage.
+    if measure_time:
+        benchmark_results.append(BenchmarkResult("time", has_failed, time_measurements))
+    if gather_stderr or gather_stdout:
+        benchmark_results.append(
+            BenchmarkResult("output", has_failed, output_measurements)
+        )
 
-    Args:
-        benchmarks: Commands divided into stages.
-        output: Set  to "stderr" to gather `stderr` or set to "stdout" to gather `stdout`.
-
-    Returns:
-        BenchmarkResult: Containing `stderr` or `stdout` of each stage. If possible, output will be converted to float.
-    """
-    has_failed = False
-
-    def _gather_stage_output(commands: list[str]) -> str | float:
-        nonlocal has_failed
-        total = ""
-        for command in commands:
-            success: bool
-            ret = ""
-            if output == "stdout":
-                ret, success = execute_and_handle_output(command, capture_stdout=True)
-            elif output == "stderr":
-                ret, success = execute_and_handle_output(command, capture_stderr=True)
-            total += ret
-
-            if not success:
-                has_failed = True
-        try:
-            return float(total)
-        except ValueError:
-            pass
-        return total
-
-    measurements: dict[str, float | str] = {}
-
-    for stage, command in benchmarks.items():
-        measurements[stage] = _gather_stage_output(command)
-    result = BenchmarkResult(output, has_failed, measurements)
-    return result
+    return benchmark_results
 
 
-def gather_stdout(benchmarks: dict[str, list[str]]) -> BenchmarkResult:
-    """Calls `_gather_output` with `output` set to "stdout"."""
-    return _gather_output(benchmarks, "stdout")
-
-
-def gather_stderr(benchmarks: dict[str, list[str]]) -> BenchmarkResult:
-    """Calls `_gather_output` with `output` set to "stderr"."""
-    return _gather_output(benchmarks, "stderr")
-
-
-def custom_metric(
-    metric_command: str, metric_name: str, benchmarks: dict[str, list[str]]
-) -> BenchmarkResult:
+def custom_metric(metric_command: str) -> dict[str, float]:
     """Execute all the benchmark commands, then execute custom metric command and process its output.
     If output has more than one line, treat output as csv file, with each column representing separate stage.
     Sum stages under `metric_name`.
@@ -114,26 +77,16 @@ def custom_metric(
     Returns:
         BenchmarkResult: Containing single or multi stage result.
     """
-    has_failed = False
-    for _, commands in benchmarks.items():
-        for command in commands:
-            _, success = execute_and_handle_output(command)
-            if not success:
-                has_failed = True
     process = execute_command(metric_command)
     output = handle_output(process, capture_stdout=True)
     process.wait()
 
     if len(output.splitlines()) == 1:
         try:
-            return BenchmarkResult(
-                metric_name, has_failed, {list(benchmarks.items())[0][0]: float(output)}
-            )
+            return {"result": float(output)}
         except ValueError:
-            pass
-        return BenchmarkResult(
-            metric_name, has_failed, {list(benchmarks.items())[0][0]: output}
-        )
+            logger.error("Conversion of '{output}' to float failed.'")
+            return None
     elif len(output.splitlines()) == 2:
         output_stream = StringIO(output)
         reader = DictReader(output_stream)
@@ -146,10 +99,10 @@ def custom_metric(
             try:
                 value = float(value)
             except ValueError:
-                pass
+                logger.error("Conversion of '{output}' to float failed.'")
+                return None
             output_dict[stage] = value
-        result = BenchmarkResult(metric_name, has_failed, output_dict)
-        return result
+        return output_dict
     else:
         logger.critical("Invalid custom metric output format:")
         logger.critical(output)
