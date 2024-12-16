f85ee352f6a62c3947946197a6a1ed7c091182b1
Maciej Janicki <mjanicki@internships.antmicro.com>
2024-12-16 14:10:47 +0100

    Faster sleeper
    More advanced sleeper
    
    Faster sleeper

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 build: main.c
 	gcc -o sleeper main.c
+	date +%s
+	>&2 echo 10
 clean:
 	rm sleeper
--- main.c
+++ main.c
@@ -1,18 +1,21 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, const char **argv) {
   int n_threads = atoi(argv[1]);
   int sleep_time;
   if(strcmp(argv[2],"data1") == 0) {
-    sleep_time = 1400000;
+    sleep_time = 1000000;
   } else if(strcmp(argv[2],"data2") == 0) { 
-    sleep_time = 520000;
+    sleep_time = 500000;
   } else if(strcmp(argv[2],"data3") == 0) { 
-    sleep_time = 1500000;
+    sleep_time = 1000000;
   }
   usleep(sleep_time/n_threads);
+  printf("%f", ((float)sleep_time/n_threads)/1e6);
+  fprintf(stderr, "%f", (float)sleep_time/1e6);
   return 0;
 }
 
