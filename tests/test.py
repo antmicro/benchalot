989295ec88503f7131ed4fe21e92a86486dc18dd
Maciej Janicki <mjanicki@internships.antmicro.com>
2025-01-14 11:43:23 +0100

    Refactore: move all unittests into a single file

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,14 @@
 import unittest
+import pandas as pd
 from benchmarker.interpolate import interpolate_variables, create_variable_combinations
+from benchmarker.output import get_combination_filtered_dfs
 
 
 def format_name(var_name: str):
     return "{{" + var_name + "}}"
 
 
-class TestInterpolation(unittest.TestCase):
+class TestInterpoleVariables(unittest.TestCase):
     def test_single_var(self):
         var_name = format_name("name")
         command = f"echo {var_name}"
@@ -54,7 +56,7 @@ class TestInterpolation(unittest.TestCase):
         self.assertEqual(cm.exception.code, 1)
 
 
-class TestCombinations(unittest.TestCase):
+class TestCreateVariableCombinations(unittest.TestCase):
 
     def test_combination_simple(self):
         matrix = {"a": [1, 2, 3], "b": [1, 2, 3]}
@@ -86,6 +88,51 @@ class TestCombinations(unittest.TestCase):
             {"v1": {"a": 1, "b": 2}, "v2": {"c": 3, "d": 4}},
             {"v1": {"a": 3, "b": 4}, "v2": {"c": 1, "d": 2}},
             {"v1": {"a": 3, "b": 4}, "v2": {"c": 3, "d": 4}},
+        ]
+        for mc in man_comb:
+            self.assertTrue(any(mc == c for c in comb))
+
+
+class TestCombinationFilteredDf(unittest.TestCase):
+    def test_simple_comb(self):
+        data = pd.DataFrame({"a": [1, 2, 3], "b": [3, 2, 1]})
+        comb = []
+        for c, row in get_combination_filtered_dfs(data, ["a", "b"]):
+            self.assertEqual(c["a"], row["a"].iloc[0])
+            self.assertEqual(c["b"], row["b"].iloc[0])
+            comb.append(c)
+        self.assertEqual(len(comb), 3)
+        man_comb = [
+            {"a": 1, "b": 3},
+            {"a": 2, "b": 2},
+            {"a": 3, "b": 1},
+        ]
+        for mc in man_comb:
+            self.assertTrue(any(mc == c for c in comb))
+
+    def test_compound_comb(self):
+        data = pd.DataFrame(
+            {
+                "v1.a": [1, 2, 3],
+                "v1.b": [4, 5, 6],
+                "v2.c": [7, 8, 9],
+                "v2.d": [10, 11, 12],
+            }
+        )
+        comb = []
+        for c, row in get_combination_filtered_dfs(
+            data, ["v1.a", "v1.b", "v2.c", "v2.d"]
+        ):
+            self.assertEqual(c["v1"]["a"], row["v1.a"].iloc[0])
+            self.assertEqual(c["v1"]["b"], row["v1.b"].iloc[0])
+            self.assertEqual(c["v2"]["c"], row["v2.c"].iloc[0])
+            self.assertEqual(c["v2"]["d"], row["v2.d"].iloc[0])
+            comb.append(c)
+        self.assertEqual(len(comb), 3)
+        man_comb = [
+            {"v1": {"a": 1, "b": 4}, "v2": {"c": 7, "d": 10}},
+            {"v1": {"a": 2, "b": 5}, "v2": {"c": 8, "d": 11}},
+            {"v1": {"a": 3, "b": 6}, "v2": {"c": 9, "d": 12}},
         ]
         for mc in man_comb:
             self.assertTrue(any(mc == c for c in comb))
