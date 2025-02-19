445fc6fb0b838cdbcbecdf3d5d495ef381a32e4a
Maciej Janicki <mjanicki@internships.antmicro.com>
2025-02-19 11:05:20 +0100

    Shorten wait time

--- main.c
+++ main.c
@@ -7,11 +7,11 @@ int main(int argc, const char **argv) {
   int n_threads = atoi(argv[1]);
   int sleep_time;
   if(strcmp(argv[2],"data1") == 0) {
-    sleep_time = 1200000;
+    sleep_time = 12000;
   } else if(strcmp(argv[2],"data2") == 0) { 
-    sleep_time = 520000;
+    sleep_time = 5200;
   } else if(strcmp(argv[2],"data3") == 0) { 
-    sleep_time = 1020000;
+    sleep_time = 10200;
   }
   usleep(sleep_time/n_threads);
   printf("%f", ((float)sleep_time/n_threads)/1e6);
